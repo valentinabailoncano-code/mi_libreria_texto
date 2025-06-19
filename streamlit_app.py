46b7876ad70b5b45f2b7b44de6bebd361d547e39
Valentina Bailon Cano <68650963+valentinabailoncano-code@users.noreply.github.com>
2025-06-19 20:23:17 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,4 @@
 # streamlit_app.py - Visualización web para MASTER-EVOLVE-MODULO-2
-import nltk
-
-# Para evitar error en Streamlit Cloud, fuerza descarga si no existe
-try:
-    nltk.data.find('corpora/stopwords')
-except LookupError:
-    nltk.download('stopwords')
-
 import streamlit as st
 import matplotlib.pyplot as plt
 import pandas as pd
